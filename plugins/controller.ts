22b6b0b341a853c2b67fc01b319d433220a9d875
Waradu <waradu@outlook.com>
2024-04-05 08:30:32 +0200

    removed pinia

--- plugins/controller.ts
+++ plugins/controller.ts
@@ -1,16 +1,6 @@
-import { createPinia } from 'pinia';
-import { useControllerStore } from '~/store/controller';
-
 export default defineNuxtPlugin((nuxtApp) => {
-  const controllerStore = useControllerStore();
-
   const controller = {
-    increment() {
-      controllerStore.increment()
-    },
-    getCounter() {
-      return controllerStore.count;
-    }
+    
   }
 
   return {
