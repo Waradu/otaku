46b1c1d0cdd9ab546382eaeb0e71e2e01b79d332
Waradu <waradu@outlook.com>
2024-04-11 13:55:10 +0200

    - fixed dev window shoind in taskbar - fix app name - added dummy tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::SystemTray;
 
 fn main() {
+  let tray = SystemTray::new();
+  
   tauri::Builder::default()
+    .system_tray(tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
