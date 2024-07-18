2fedb1dacf178bfcaa3999eb60a81b1ee7bda139
Waradu <waradu@outlook.com>
2024-07-18 16:16:36 +0200

    added compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false
-})
+  ssr: false,
+  compatibilityDate: "2024-01-01",
+});
